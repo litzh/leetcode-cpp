3511933b68efb7e37673a23fc1e3e491450a5d20
litzh <litzh@foxmail.com>
2024-01-01 00:35:18 +0800

    LC0015 3Sum two pointer

--- src/lc0015-3-sum.cpp
+++ src/lc0015-3-sum.cpp
@@ -1,10 +1,42 @@
 #include <algorithm>
+#include <cstddef>
 #include <set>
 #include <unordered_map>
 #include <vector>
 namespace lc0015 {
 
 std::vector<std::vector<int>> threeSum(const std::vector<int> &nums) {
+    std::vector<int> sorted(nums);
+    std::sort(sorted.begin(), sorted.end());
+    std::vector<std::vector<int>> result;
+    size_t i = 0;
+    while (i < sorted.size()) {
+        size_t l = i + 1, r = sorted.size() - 1;
+        int num = sorted[i];
+        while (l < r) {
+            int left = sorted[l], right = sorted[r];
+            int sum = num + left + right;
+            if (sum < 0) {
+                while (l < r && left == sorted[++l])
+                    ;
+            } else if (sum > 0) {
+                while (l < r && right == sorted[--r])
+                    ;
+            } else {
+                result.push_back({num, left, right});
+                while (l < r && left == sorted[++l])
+                    ;
+                while (l < r && right == sorted[--r])
+                    ;
+            }
+        }
+        while (++i < sorted.size() && num == sorted[i])
+            ;
+    }
+    return result;
+}
+
+std::vector<std::vector<int>> threeSumCount(const std::vector<int> &nums) {
     std::unordered_map<int, int> num_count;
     for (const auto &i: nums) {
         if (num_count.find(i) != num_count.end())
--- test/lc0015.cpp
+++ test/lc0015.cpp
@@ -3,7 +3,8 @@
 
 namespace lc0015 {
 extern std::vector<std::vector<int>> threeSum(const std::vector<int> &nums);
-}
+extern std::vector<std::vector<int>> threeSumCount(const std::vector<int> &nums);
+} // namespace lc0015
 
 
 using namespace lc0015;
@@ -15,4 +16,12 @@ TEST_CASE("3Sum", "[LC0015]") {
     REQUIRE(threeSum("[0,0,0]"_VI) == "[[0,0,0]]"_VVI);
     REQUIRE(threeSum("[-2,2,0,1,-1]"_VI) == "[[-2,0,2],[-1,0,1]]"_VVI);
 }
- 
+
+TEST_CASE("3Sum Count", "[LC0015]") {
+    REQUIRE(threeSumCount("[1,2,3]"_VI) == "[]"_VVI);
+    REQUIRE(threeSumCount("[]"_VI) == "[]"_VVI);
+    REQUIRE(threeSumCount("[-1,0,1,2,-1,-4]"_VI) == "[[-1,-1,2],[-1,0,1]]"_VVI);
+    REQUIRE(threeSumCount("[0,0,0]"_VI) == "[[0,0,0]]"_VVI);
+    REQUIRE(threeSumCount("[-2,2,0,1,-1]"_VI) == "[[-2,0,2],[-1,0,1]]"_VVI);
+}
+
