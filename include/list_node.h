9511ab7ddd8b89cd822519e0493c3e5c9c5e115f
litzh <litzh@foxmail.com>
2024-01-01 22:59:54 +0800

    list_node member function

--- include/list_node.h
+++ include/list_node.h
@@ -13,31 +13,29 @@ struct ListNode {
     ListNode(const int x, ListNode *next)
         : val(x)
         , next(next) {}
-};
-
-static ListNode *list_from_vector(const std::vector<int> &vec) {
-    ListNode *head = nullptr;
-    ListNode *tail = nullptr;
-    for (auto &val: vec) {
-        if (tail == nullptr) {
-            tail = new ListNode(val);
-            head = tail;
-        } else {
-            tail->next = new ListNode(val);
-            tail = tail->next;
+    explicit ListNode(const std::vector<int> &vec) {
+        ListNode *tail = nullptr;
+        for (auto &val: vec) {
+            if (tail == nullptr) {
+                this->val = val;
+                this->next = nullptr;
+                tail = this;
+            } else {
+                tail->next = new ListNode(val);
+                tail = tail->next;
+            }
         }
     }
-    return head;
-}
-
-static std::vector<int> list_to_vector(const ListNode *head) {
-    std::vector<int> vec;
-    while (head != nullptr) {
-        vec.push_back(head->val);
-        head = head->next;
+    std::vector<int> v() const {
+        ListNode const *head = this;
+        std::vector<int> vec;
+        while (head != nullptr) {
+            vec.push_back(head->val);
+            head = head->next;
+        }
+        return vec;
     }
-    return vec;
-}
+};
 
 static void list_freep(ListNode **headp) {
     const ListNode *head = *headp;
--- test/lc0002.cpp
+++ test/lc0002.cpp
@@ -7,28 +7,28 @@ extern ListNode *addTwoNumbers(const ListNode *l1, const ListNode *l2);
 }
 using namespace lc0002;
 TEST_CASE("Add Two Numbers", "[LC0002]") {
-    ListNode *l1 = list_from_vector("[2, 4, 3]"_VI);
-    ListNode *l2 = list_from_vector("[5, 6, 4]"_VI);
+    ListNode *l1 = new ListNode("[2, 4, 3]"_VI);
+    ListNode *l2 = new ListNode("[5, 6, 4]"_VI);
     ListNode *l3 = addTwoNumbers(l1, l2);
-    REQUIRE(list_to_vector(l3) == "[7, 0, 8]"_VI);
+    REQUIRE(l3->v() == "[7, 0, 8]"_VI);
     list_freep(&l1);
     list_freep(&l2);
     list_freep(&l3);
 }
 
 TEST_CASE("Dangling Carry", "[LC0002]") {
-    ListNode *l1 = list_from_vector("[9, 9, 9, 9, 9, 9, 9]"_VI);
-    ListNode *l2 = list_from_vector("[9, 9, 9, 9]"_VI);
+    ListNode *l1 = new ListNode("[9, 9, 9, 9, 9, 9, 9]"_VI);
+    ListNode *l2 = new ListNode("[9, 9, 9, 9]"_VI);
     ListNode *l3 = addTwoNumbers(l1, l2);
-    REQUIRE(list_to_vector(l3) == "[8, 9, 9, 9, 0, 0, 0, 1]"_VI);
+    REQUIRE(l3->v() == "[8, 9, 9, 9, 0, 0, 0, 1]"_VI);
     list_freep(&l1);
     list_freep(&l2);
     list_freep(&l3);
 
-    l1 = list_from_vector("[9, 9, 9, 9]"_VI);
-    l2 = list_from_vector("[9, 9, 9, 9, 9, 9, 9]"_VI);
+    l1 = new ListNode("[9, 9, 9, 9]"_VI);
+    l2 = new ListNode("[9, 9, 9, 9, 9, 9, 9]"_VI);
     l3 = addTwoNumbers(l1, l2);
-    REQUIRE(list_to_vector(l3) == "[8, 9, 9, 9, 0, 0, 0, 1]"_VI);
+    REQUIRE(l3->v() == "[8, 9, 9, 9, 0, 0, 0, 1]"_VI);
     list_freep(&l1);
     list_freep(&l2);
     list_freep(&l3);
