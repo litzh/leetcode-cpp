fc8636a9a32b8125b9da6be4fa906bfe8ec6619f
litzh <litzh@foxmail.com>
2024-01-01 14:09:35 +0800

    LC0015 branch coverage

--- test/lc0015.cpp
+++ test/lc0015.cpp
@@ -11,9 +11,13 @@ using namespace lc0015;
 
 TEST_CASE("3Sum", "[LC0015]") {
     REQUIRE(threeSum("[1,2,3]"_VI) == "[]"_VVI);
+    REQUIRE(threeSum("[-1,-1,-1,-1,-1]"_VI) == "[]"_VVI);
     REQUIRE(threeSum("[]"_VI) == "[]"_VVI);
     REQUIRE(threeSum("[-1,0,1,2,-1,-4]"_VI) == "[[-1,-1,2],[-1,0,1]]"_VVI);
     REQUIRE(threeSum("[0,0,0]"_VI) == "[[0,0,0]]"_VVI);
+    REQUIRE(threeSum("[-1,0,0,1,2,2,2]"_VI) == "[[-1,0,1]]"_VVI);
+    REQUIRE(threeSum("[-1,0,0,1,1,1,1]"_VI) == "[[-1,0,1]]"_VVI);
+    REQUIRE(threeSum("[-2,-2,0,0,0,1]"_VI) == "[[0,0,0]]"_VVI);
     REQUIRE(threeSum("[-2,2,0,1,-1]"_VI) == "[[-2,0,2],[-1,0,1]]"_VVI);
 }
 
