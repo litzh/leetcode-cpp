ce9aae63fa6d8a618a54938960c4b0a55ef473c0
litzh <litzh@foxmail.com>
2024-01-06 13:35:10 +0800

    LC0033 Search in Rotated Sorted Array

--- src/lc0033-search-in-rotated-sorted-array.cpp
+++ src/lc0033-search-in-rotated-sorted-array.cpp
@@ -1,16 +1,60 @@
+#include <cassert>
 #include <cstddef>
+#include <cstdio>
 #include <vector>
 namespace lc0033 {
 // 1 <= nums.length <= 5000
 // -1000 <= nums[i], target <= 1000
 // All values of nums are unique.
 // nums is an ascending array that is possibly rotated.
-int search(const std::vector<int> &nums, int target) {
-    // [i+1,i+2,...,n][0,1,...,i]
-    // 查找 n 的位置
+int search_min(const std::vector<int> &nums) {
+    assert(nums.size() >= 1 && nums.size() <= 5000);
     size_t size = nums.size();
-    size_t mid = size / 2;
-    return target;
+    int end = nums[size - 1];
+    size_t l = 0, r = size - 1;
+    size_t min = l;
+    while (nums[l] > nums[r]) {
+        size_t m = l + (r - l) / 2;
+        assert(l < r && l <= m);
+        if (m == l) {
+            min = r;
+            break;
+        }
+        if (nums[m] <= end) {
+            r = m;
+        } else {
+            l = m;
+        }
+        min = l;
+    }
+    return min;
+}
+
+int search(const std::vector<int> &nums, int target) {
+    assert(nums.size() >= 1 && nums.size() <= 5000);
+    size_t size = nums.size();
+    int end = nums[size - 1];
+    size_t min = search_min(nums);
+    size_t lower = min, upper = size - 1;
+    if (target > end) {
+        lower = 0, upper = min > 0 ? min - 1 : 0;
+    }
+    while (lower <= upper) {
+        size_t mid = lower + (upper - lower) / 2;
+        int num = nums[mid];
+        if (num == target) {
+            return static_cast<int>(mid);
+        } else if (num > target) {
+            if (mid == 0)
+                break;
+            upper = mid - 1;
+        } else {
+            if (mid >= size - 1)
+                break;
+            lower = mid + 1;
+        }
+    }
+    return -1;
 }
 } // namespace lc0033
 
--- test/lc0033.cpp
+++ test/lc0033.cpp
@@ -4,13 +4,57 @@
 #include <catch2/catch_test_macros.hpp>
 
 namespace lc0033 {
+int search_min(const std::vector<int> &nums);
 int search(const std::vector<int> &nums, int target);
 } // namespace lc0033
 
 using namespace lc0033;
+
+TEST_CASE("Search in Rotated Sorted Array Min", "[LC0033]") {
+    REQUIRE(search_min("[1]"_VI) == 0);
+    REQUIRE(search_min("[1,2]"_VI) == 0);
+    REQUIRE(search_min("[2,1]"_VI) == 1);
+    REQUIRE(search_min("[1,2,3]"_VI) == 0);
+    REQUIRE(search_min("[3,1,2]"_VI) == 1);
+    REQUIRE(search_min("[2,3,1]"_VI) == 2);
+    REQUIRE(search_min("[1,2,3,4]"_VI) == 0);
+    REQUIRE(search_min("[4,1,2,3]"_VI) == 1);
+    REQUIRE(search_min("[3,4,1,2]"_VI) == 2);
+    REQUIRE(search_min("[2,3,4,1]"_VI) == 3);
+}
+
 TEST_CASE("Search in Rotated Sorted Array", "[LC0033]") {
-    REQUIRE(search("[4,5,6,7,0,1,2]"_VI, 0) == 4);
-    REQUIRE(search("[4,5,6,7,0,1,2]"_VI, 3) == -1);
     REQUIRE(search("[1]"_VI, 0) == -1);
+    REQUIRE(search("[1]"_VI, 1) == 0);
+    REQUIRE(search("[1]"_VI, 2) == -1);
+    REQUIRE(search("[3,5,1]"_VI, 5) == 1);
+    REQUIRE(search("[4,5,6,7,0,1,2]"_VI, 4) == 0);
+    REQUIRE(search("[4,5,6,7,0,1,2]"_VI, 5) == 1);
+    REQUIRE(search("[4,5,6,7,0,1,2]"_VI, 6) == 2);
+    REQUIRE(search("[4,5,6,7,0,1,2]"_VI, 7) == 3);
+    REQUIRE(search("[4,5,6,7,0,1,2]"_VI, 0) == 4);
+    REQUIRE(search("[4,5,6,7,0,1,2]"_VI, 1) == 5);
+    REQUIRE(search("[4,5,6,7,0,1,2]"_VI, 2) == 6);
+    REQUIRE(search("[4,5,6,7,0,1,2]"_VI, 3) == -1);
+    REQUIRE(search("[4,5,6,7,0,1,2]"_VI, 8) == -1);
+    REQUIRE(search("[1,2,3,4,5,7]"_VI, 0) == -1);
+    REQUIRE(search("[1,2,3,4,5,7]"_VI, 1) == 0);
+    REQUIRE(search("[1,2,3,4,5,7]"_VI, 2) == 1);
+    REQUIRE(search("[1,2,3,4,5,7]"_VI, 3) == 2);
+    REQUIRE(search("[1,2,3,4,5,7]"_VI, 4) == 3);
+    REQUIRE(search("[1,2,3,4,5,7]"_VI, 5) == 4);
+    REQUIRE(search("[1,2,3,4,5,7]"_VI, 6) == -1);
+    REQUIRE(search("[1,2,3,4,5,7]"_VI, 7) == 5);
+    REQUIRE(search("[1,2,3,4,5,7]"_VI, 8) == -1);
+    REQUIRE(search("[5,1,3]"_VI, 5) == 0);
+    REQUIRE(search("[5,1,3]"_VI, 1) == 1);
+    REQUIRE(search("[5,1,3]"_VI, 3) == 2);
+    REQUIRE(search("[5,1,3]"_VI, 0) == -1);
+    REQUIRE(search("[5,1,3]"_VI, 6) == -1);
+    REQUIRE(search("[3,1]"_VI, 0) == -1);
+    REQUIRE(search("[3,1]"_VI, 3) == 0);
+    REQUIRE(search("[3,1]"_VI, 1) == 1);
+    REQUIRE(search("[3,1]"_VI, 2) == -1);
+    REQUIRE(search("[3,1]"_VI, 4) == -1);
 }
 
